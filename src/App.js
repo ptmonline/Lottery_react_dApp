d5d3f2235088a36cd28a0dc8d6a44bde42b6313a
ptmonline <ptmonlinework@gmail.com>
2019-02-08 16:53:12 +0100

    added from to enter lottery contest

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { manager: '' };
-  }
+  //   this.state = { manager: '' };
+  // }
+
+  state = {
+    manager: '',
+    players: [],
+    balance: '',
+    value: ''
+  };
+
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager });
+    this.setState({ manager, players, balance });
   }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+  }
+
   render() {
     web3.eth.getAccounts().then(console.log);
     console.log(web3.version);
@@ -22,7 +38,21 @@ class App extends Component {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>This contract is managed by {this.state.manager}.</p>
+        <p>There are currently {this.state.players.length} people entered,
+        competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4>Want to try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter: </label>
+            <input
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value })}
+            />
+            <button>Enter</button>
+          </div>
+        </form>
       </div>
     );
   }
