3a81f715fd6903a8aa612d1e06fddb34265bdc07
ptmonline <ptmonlinework@gmail.com>
2019-02-08 16:33:15 +0100

    displayed manager on app

--- src/lottery.js
+++ src/lottery.js
@@ -3,3 +3,5 @@ import web3 from './web3';
 const address = '0xfCD3AE0F47c5aB7546ACe1cd3FB5C544be086C27';
 
 const abi = [{ "constant": true, "inputs": [], "name": "manager", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "pickWinner", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getPlayers", "outputs": [{ "name": "", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "enter", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "players", "outputs": [{ "name": "", "type": "address" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }]
+
+export default new web3.eth.Contract(abi, address);
